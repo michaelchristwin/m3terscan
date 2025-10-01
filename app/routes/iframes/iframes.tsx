3fc48bb9dacf4ab7ae009d7668c1c8bccecb7914
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-10-01 15:09:37 +0100

    Chore: accepting color scheme as params

--- app/routes/iframes/iframes.tsx
+++ app/routes/iframes/iframes.tsx
@@ -1,4 +1,15 @@
-import { Outlet } from "react-router";
+import { data, Outlet } from "react-router";
+import { setColorScheme } from "~/.server/cookies";
+import type { Route } from "./+types/iframes";
+
+export async function action({ request }: Route.ActionArgs) {
+  const url = new URL(request.url);
+  const colorScheme = url.searchParams.get("colorScheme");
+
+  return data(null, {
+    headers: { "Set-Cookie": await setColorScheme(colorScheme || "light") },
+  });
+}
 
 function Iframes() {
   return <Outlet />;
